759e1ce72e46662c23e08227976d5de7c842ba2a
Sebastian Husche <sebastian@scanbot.io>
2023-06-06 10:45:25 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ScanbotDocumentScannerSDK",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "ScanbotDocumentScannerSDK",
+            targets: ["ScanbotDocumentScannerSDK"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ScanbotDocumentScannerSDK",
+            url: "",
+            checksum: ""
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,10 @@
+# Scanbot Document Scanner SDK for iOS
+
+Swift Package Manager repository for the [Scanbot Document Scanner SDK for iOS](https://scanbot.io).
+
+Provides functionality like [Document Scanning](https://scanbot.io/developer/ios-document-scanner/) and Image Filtering
+
+For more details please see:
+
+- [Documentation](https://docs.scanbot.io/document-scanner-sdk/ios/introduction/)
+- [Example App](https://github.com/doo/scanbot-sdk-example-ios)
